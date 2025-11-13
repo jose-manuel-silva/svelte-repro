094f25868140817466a5ad4fc256d34f27f4b592
José Silva <jose.silva.priv@gmail.com>
2025-11-13 22:57:42 +0000

    minimum repro - svelte async bug

---
+++ src/lib/components/app-sidebar.svelte
@@ -0,0 +1,116 @@
+<script lang="ts" module>
+	import BookOpenIcon from '@lucide/svelte/icons/book-open';
+	import BotIcon from '@lucide/svelte/icons/bot';
+	import Settings2Icon from '@lucide/svelte/icons/settings-2';
+	import SquareTerminalIcon from '@lucide/svelte/icons/square-terminal';
+
+	// This is sample data.
+	const data = {
+		navMain: [
+			{
+				title: 'Playground',
+				url: '#',
+				icon: SquareTerminalIcon,
+				isActive: true,
+				items: [
+					{
+						title: 'History',
+						url: '#'
+					},
+					{
+						title: 'Starred',
+						url: '#'
+					},
+					{
+						title: 'Settings',
+						url: '#'
+					}
+				]
+			},
+			{
+				title: 'Models',
+				url: '#',
+				icon: BotIcon,
+				items: [
+					{
+						title: 'Genesis',
+						url: '#'
+					},
+					{
+						title: 'Explorer',
+						url: '#'
+					},
+					{
+						title: 'Quantum',
+						url: '#'
+					}
+				]
+			},
+			{
+				title: 'Documentation',
+				url: '#',
+				icon: BookOpenIcon,
+				items: [
+					{
+						title: 'Introduction',
+						url: '#'
+					},
+					{
+						title: 'Get Started',
+						url: '#'
+					},
+					{
+						title: 'Tutorials',
+						url: '#'
+					},
+					{
+						title: 'Changelog',
+						url: '#'
+					}
+				]
+			},
+			{
+				title: 'Settings',
+				url: '#',
+				icon: Settings2Icon,
+				items: [
+					{
+						title: 'General',
+						url: '#'
+					},
+					{
+						title: 'Team',
+						url: '#'
+					},
+					{
+						title: 'Billing',
+						url: '#'
+					},
+					{
+						title: 'Limits',
+						url: '#'
+					}
+				]
+			}
+		]
+	};
+</script>
+
+<script lang="ts">
+	import NavMain from './nav-main.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import type { ComponentProps } from 'svelte';
+
+	let {
+		ref = $bindable(null),
+		collapsible = 'icon',
+		...restProps
+	}: ComponentProps<typeof Sidebar.Root> = $props();
+</script>
+
+<Sidebar.Root {collapsible} {...restProps}>
+	<Sidebar.Content>
+		<NavMain items={data.navMain} />
+	</Sidebar.Content>
+	<Sidebar.Rail />
+</Sidebar.Root>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { resolve } from '$app/paths';
+</script>
+
+<a
+	href={resolve('/[testID]', {
+		testID: '1'
+	})}>Test 1</a
+>
+
+<a
+	href={resolve('/[testID]', {
+		testID: '2'
+	})}>Test 2</a
+>
